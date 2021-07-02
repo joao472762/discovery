8810bd6085afd06cde75d3589f32e5dd623b0e04
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2021-07-02 14:03:00 -0300

    javinha

--- script.js
+++ script.js
@@ -6,3 +6,88 @@ const modal = {
         document.querySelector('.modal-overlay').classList.remove('active')
     }
 }
+
+var transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        value: -50000,
+        date: '30/03/2021'
+    },
+    {
+        id:2,
+        description: 'Criação de Web site',
+        value: 500000,
+        date:'02/42/2021'
+    },
+    {
+        id:3,
+        description: 'Internet',
+        value: -20000,
+        date:'28/02/2021'
+    },
+    {
+        id: 4,
+        description: 'Agua',
+        value: -6000,
+        date:'28/04/2021'
+    }
+]
+let Transaction = {
+    incomes(){
+        // tem a função de somar todas as entradas 
+    },
+    spend(){
+        // tem a função de somar todos os gastos
+    },
+    total(){
+        // tem a função ser o total entre os meus gastos e as minhas entradas
+    }
+}
+const DOM = {
+    ContainerTransaction: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index){
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHtmlTransaction(transaction)
+        DOM.ContainerTransaction.appendChild(tr)
+    },
+    innerHtmlTransaction(transaction) {
+        const Cssclass = transaction.value > 0 ? 'gain' : "spend"
+
+        const amount = Utils.formatCurrecy(transaction.value)
+        let html = `
+                <td>
+                    ${transaction.description}
+                </td>
+                <td class="${Cssclass}">
+                    ${transaction.value}
+                </td>
+                <td>
+                    ${transaction.date}
+                </td>
+                <td>
+                    <img src="assets/minus.svg" alt="remover">
+                </td>
+            
+                    `
+                    return html
+                }
+}
+for(var cont = 0; cont <= transactions.length; cont ++){
+    DOM.addTransaction(transactions[cont])
+}
+console.log('eu estou fazendo um progama para ganahar uma boa grana e para paguar de bacana')
+
+const Utils = {
+    formatCurrecy(value) {
+         const signal = Number(value) < 0 ? "-" : ""
+
+         value = String(value).replace(/\D/g, "")
+
+         value = Number(value) / 100
+
+         value = value
+
+    }
+}
